c5083e8ed744110ffe68789bab97d680e6bd69e9
koritacrah <juanmcoro2003@gmail.com>
2021-06-02 19:05:03 -0300

    agregar html PoI
    se agrego el html PoI yy se actualizo el controller

--- TrabajoPracticoFinalCoroGimenezRochaCancinos/src/main/java/ar/edu/unju/edm/controller/PoIsController.java
+++ TrabajoPracticoFinalCoroGimenezRochaCancinos/src/main/java/ar/edu/unju/edm/controller/PoIsController.java
@@ -1,5 +1,27 @@
 package ar.edu.unju.edm.controller;
 
-public class PoIsController {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import ar.edu.unju.edm.service.IPoIsService;
+
+public class PoIsController {
+	private static final Log LOGGER = LogFactory.getLog(PoIsController.class);
+	
+	@Autowired
+	@Qualifier("implementacionMYSQLPoI")
+	
+	IPoIsService poiService;
+
+	@GetMapping("/cargarPoI")
+	public String crearPoI(Model model) {
+		LOGGER.info("METHOD: ingresando el metodo cargar");
+		model.addAttribute("PoI", poiService.crearPoI());
+
+		return ("PoI");
+	}
 }
